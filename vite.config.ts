7ec8876d1da9c74b5c2db8de5a00ceb507f8df87
David Moorhouse <david@locusinnovations.com.au>
2026-01-27 17:05:35 +1100

    chunking config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          ui: ['@mui/material', '@mui/icons-material'],
+          three: ['three'],
+          examples: ['three/examples/jsm/controls/OrbitControls.js', 'three/examples/jsm/loaders/SVGLoader.js'],
+          fiber: ['@react-three/fiber', '@react-three/drei'],
+        }
+      }
+    }
+  }
 })
