b1242783ed0bc99639395e82c6cbeea8de2f0a18
David Moorhouse <david@locusinnovations.com.au>
2026-02-03 15:11:57 +1100

    dynamic grid resizing

--- src/components/CentralDisplay.tsx
+++ src/components/CentralDisplay.tsx
@@ -10,24 +10,32 @@ export function CentralDisplay(props:
     loading: boolean, 
     volumes: Volume[], 
     margins: number, 
+    rightDrawerOpen: boolean,
     zScale: number,
     handleClickSelect: (name: string, selected: boolean) => void,
     handleClearSelection: () => void}){
-  const { width } = useWindowSize();
+  const { width, height } = useWindowSize();
   
-  const padding = 30
-  const drawerWidth = props.margins + padding
-  const marginLeft = `${drawerWidth-10}px`
-  const marginRight = `${width - drawerWidth- padding}px`
-  const modelSize = {width: 2500, height: 2500 }
+  const padding = 10
+  const drawerWidth = 320
+  const toolbarHeight = 80
+
+  const rightMarginSize = props.rightDrawerOpen ? drawerWidth : padding
+  const marginLeft = `${drawerWidth + padding}px`
+  const marginRight = `${rightMarginSize}px`
+  const marginTop = `${toolbarHeight + padding}px`
+  const marginBottom = `${padding}px`
+  const availableWidth = Math.max(300, width - drawerWidth - rightMarginSize - padding * 2)
+  const availableHeight = Math.max(300, height - toolbarHeight - padding * 2)
+  const modelSize = { width: availableWidth, height: availableHeight }
 
   return (
     <Box
       component="main"
-      sx={{ ml: marginLeft, mr: marginRight, marginTop: `64px`, marginBottom: `${padding}px` }}
+      sx={{ ml: marginLeft, mr: marginRight, marginTop: marginTop, marginBottom: marginBottom }}
     >
         {props.loading ? <Loading />:''}
-      <ModelDisplay 
+      <ModelDisplay
         volumes={props.volumes}
         handleClickSelect={props.handleClickSelect}
         handleClearSelection={props.handleClearSelection}
